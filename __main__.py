1339d861596f5a9431788ae920336abc901bd453
kirihar2 <kirihar2@illinois.edu>
2017-12-01 16:49:44 -0600

    refactored for unit testing capabilities

--- __main__.py
+++ __main__.py
@@ -1,51 +1,5 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-from sqlalchemy import create_engine
-from flask.ext.jsonpify import jsonify
-from redis import Redis, RedisError
-import os
-import socket
+__author__ = 'jurankirihara'
+from __init__ import *
 
-db_connect = create_engine('sqlite:///chinook.db')
-app = Flask(__name__)
-api = Api(app)
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
-
-class Employees(Resource):
-    def get(self):
-        conn = db_connect.connect() # connect to database
-        query = conn.execute("select * from employees") # This line performs query and returns json result
-        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
-
-class Tracks(Resource):
-    def get(self):
-        conn = db_connect.connect()
-        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        return jsonify(result)
-@app.route("/")
-def hello():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
-
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
-
-class Employees_Name(Resource):
-    def get(self, employee_id):
-        conn = db_connect.connect()
-        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        return jsonify(result)
-        
-api.add_resource(Employees, '/employees') # Route_1
-api.add_resource(Tracks, '/tracks') # Route_2
-api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
-
-
-if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    run(80)
